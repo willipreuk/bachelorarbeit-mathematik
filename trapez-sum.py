6942b1ae31c5108f9548cb976c3dc0b547ee0f6e
Willi Preuk <willi@preuk.eu>
2024-07-31 09:53:25 +0200

    add gauss legendre

--- trapez-sum.py
+++ trapez-sum.py
@@ -25,20 +25,62 @@ def milne_sum(a, b, n, f):
     # Apply Milne's rule
     integral = 0
     for i in range(0, n, 4):
-        integral += (7*y[i] + 32*y[i+1] + 12*y[i+2] + 32*y[i+3] + 7*y[i+4])
+        integral += (7 * y[i] + 32 * y[i + 1] + 12 * y[i + 2] + 32 * y[i + 3] + 7 * y[i + 4])
 
     integral *= (4 * h / 90)
 
     return integral
 
 
+def gauss_legendre_integration(f, a, b):
+    nodes = np.array([-1 / 3 * np.sqrt(5 + 2 * np.sqrt(10 / 7)),
+                      -1 / 3 * np.sqrt(5 - 2 * np.sqrt(10 / 7)),
+                      0.0,
+                      1 / 3 * np.sqrt(5 - 2 * np.sqrt(10 / 7)),
+                      1 / 3 * np.sqrt(5 + 2 * np.sqrt(10 / 7))
+                      ])
+    weights = np.array([(322-13*np.sqrt(70))/900,
+                        (322+13*np.sqrt(17))/900,
+                        128/225,
+                        (322+13*np.sqrt(17))/900,
+                        (322-13*np.sqrt(70))/900
+                        ])
+
+    # Transform interval from [a, b] to [-1, 1]
+    def transform(x):
+        return 0.5 * (b - a) * x + 0.5 * (b + a)
+
+    integral = 0.0
+    for i in range(len(nodes)):
+        integral += weights[i] * f(transform(nodes[i]))
+
+    # Scale the result by the interval length
+    integral *= 0.5 * (b - a)
+
+    return integral
+
+
+def gauss_legendre_integration_multiple_intervals(f, a, b, num_intervals):
+    # Calculate the width of each subinterval
+    sub_interval_width = (b - a) / num_intervals
+    total_integral = 0.0
+
+    for i in range(num_intervals):
+        sub_a = a + i * sub_interval_width
+        sub_b = sub_a + sub_interval_width
+        total_integral += gauss_legendre_integration(f, sub_a, sub_b)
+
+    return total_integral
+
+
 def function(x):
-    return 1/(x+0.00001)
+    return 1 / (x + 0.00001)
 
 
-iterations = 100000
+iterations = 10000
 
-correct = sp.integrate.quad(function, 0, np.pi)[0]
+correct = sp.integrate.quad(function, 0, 1)[0]
 print("Correct value: ", correct)
-print(correct - trapez_sum(0, np.pi, iterations, function))
-print(correct - milne_sum(0, np.pi, iterations, function))
+print("Error Trapez: ", correct - trapez_sum(0, 1, iterations, function))
+print("Error Milne: ", correct - milne_sum(0, 1, iterations, function))
+print("Error Gauß-Christoffel: ", correct - gauss_legendre_integration_multiple_intervals(function, 0, 1, iterations))
