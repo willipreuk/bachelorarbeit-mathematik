84ee4d1f2aee8a0b2ddce132071728e0b255c8be
Willi Preuk <willi@preuk.eu>
2024-08-31 10:50:53 +0200

    better track model

--- track/data.py
+++ track/data.py
@@ -1,7 +1,7 @@
 from typing import Any
-
 import numpy as np
 from numpy import ndarray, dtype
+import matplotlib.pyplot as plt
 
 file_path = "data/Hhwayli.dat"
 
@@ -14,3 +14,18 @@ def read_data() -> tuple[ndarray[float, dtype[Any]], ndarray[float, dtype[Any]]]
         data = [float(x) for x in data]
 
     return np.array(data), x_vals
+
+
+def read_data_interpolated() -> tuple[ndarray[float, dtype[Any]], ndarray[float, dtype[Any]]]:
+    data, x_vals = read_data()
+    x_vals_fine = np.arange(0, 409.6, 0.005)
+
+    return np.interp(x_vals_fine, x_vals, data), x_vals_fine
+
+if __name__ == '__main__':
+    data, x_vals = read_data()
+
+    plt.figure()
+    nn_x = np.linspace(0, 409.6, 10000)
+    plt.plot(x_vals, data)
+    plt.show()
--- track/main.py
+++ track/main.py
@@ -3,13 +3,26 @@ import numpy as np
 
 from data import read_data
 from neural_network import NeuralNetwork
+from simulation.track.data import read_data_interpolated
 
 if __name__ == '__main__':
-    data, x_vals = read_data()
+    data, x_vals = read_data_interpolated()
 
     nn = NeuralNetwork(x_vals, data)
-    nn.train()
+    # nn.load()
 
+    nn.create_model()
+    history = nn.train()
+
+    plt.figure()
+    plt.plot(history.history['loss'], label='Training Loss')
+    plt.plot(history.history['val_loss'], label='Validation Loss')
+    plt.title('Model Loss Over Epochs')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+
+    plt.figure()
     nn_x = np.linspace(0, 409.6, 10000)
     plt.plot(nn_x, nn.predict(nn_x))
     plt.plot(x_vals, data, 'x')
